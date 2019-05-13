1c961acb872daed7b3ebe8cc4c98187a81c05f8f
vgeorgiev90 <v.georgiev90@abv.bg>
2019-05-13 13:03:04 +0300

    small fix

--- ha-infrastructure-with-k8s-and-webapp/deploy-systemd-cluster/roles/post/templates/deploy.sh
+++ ha-infrastructure-with-k8s-and-webapp/deploy-systemd-cluster/roles/post/templates/deploy.sh
@@ -14,6 +14,19 @@ kubectl create -f /root/post-install/grafana-ingress.yml
 POD=$(kubectl get pods -n kube-system | grep heketi | awk '{print $1}')
 kubectl -n kube-system exec -it ${POD} -- cat /root/.ssh/id_rsa.pub
 ;;
+'heketi-cluster')
+if [ $# -eq 3 ];then
+POD=$(kubectl get pods -n kube-system | grep heketi | awk '{print $1}')
+kubectl -n kube-system exec -it ${POD} -- sed -i "s/node1/${2}/g" /etc/heketi/topology.json
+kubectl -n kube-system exec -it ${POD} -- sed -i "s/node2/${3}/g" /etc/heketi/topology.json
+kubectl -n kube-system exec -it ${POD} -- sed -i 's/\/dev\/vdb/\/dev\/xvdf/g' /etc/heketi/topology.json
+kubectl -n kube-system exec -it ${POD} -- heketi-cli topology load --json=/etc/heketi/topology.json
+echo ""
+kubectl -n kube-system exec -it ${POD} -- heketi-cli cluster list
+else
+echo "Usage: ./deploy.sh heketi-cluster NODE1-PRIVATE-IP NODE2-PRIVATE-IP"
+fi
+;;
 'deploy-app')
 kubectl create -f /root/post-install/wordpress-application.yml
 sleep 30
@@ -23,6 +36,7 @@ kubectl exec -it web-0 -- /root/deploy.sh
 echo "Usage:"
 echo "ingress        -- create ingress resources for weave ,dashboard and monitoring" 
 echo "get-key        -- Get the public key for heketi container which must be distributed on the worker nodes before topology load" 
-echo "deploy-app -- Deploy wordpress website on the web container after it is initialized"
+echo "deploy-app     -- Deploy wordpress website on the web container after it is initialized"
+echo "heketi-cluster -- Bootstrap heketi cluster , after public key is distributed accross the worker nodes"
 esac
 
