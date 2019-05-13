a930a04539643a41f91008ca90f92a1e1c7f3586
Viktor Georgiev <v.georgiev90@abv.bg>
2019-05-13 16:58:33 +0300

    Update get-python.sh

--- ha-infrastructure-with-k8s-and-webapp/support/get-python.sh
+++ ha-infrastructure-with-k8s-and-webapp/support/get-python.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
-if [ $# -eq 3 ];then
-
-for i in ${1} ${2} ${3};do
-	ssh -t ubuntu@${i} 'sudo apt-get install python-minimal -y'
+if [ $# -gt 1 ];then
+for i in $@;do
+        ssh -t ubuntu@${i} 'sudo apt-get install python-minimal -y'
 done
-
 else
-	echo "Usage: ./get-python.sh IP1 IP2 IP3"
+        echo "Usage: ./get-python.sh IP1 IP2 IP3 .. .."
 fi
+
